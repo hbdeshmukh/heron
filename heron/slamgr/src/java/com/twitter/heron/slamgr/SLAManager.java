c978b846728d2b1f5b30f2b7c930f1973ccf8542
Ashvin Agrawal <ashvin@apache.org>
2016-11-03 14:20:17 -0700

    SLA Manager demo implementation

--- heron/slamgr/src/java/com/twitter/heron/slamgr/SLAManager.java
+++ heron/slamgr/src/java/com/twitter/heron/slamgr/SLAManager.java
@@ -14,11 +14,238 @@
 
 package com.twitter.heron.slamgr;
 
-import com.twitter.heron.slamgr.sinkvisitor.TrackerVisitor;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+
+import com.twitter.heron.common.utils.logging.LoggingHelper;
+import com.twitter.heron.spi.common.ClusterConfig;
+import com.twitter.heron.spi.common.ClusterDefaults;
+import com.twitter.heron.spi.common.Config;
+import com.twitter.heron.spi.common.Keys;
 
 public class SLAManager {
+  private static final Logger LOG = Logger.getLogger(SLAManager.class.getName());
+  private final Config config;
 
-  private TrackerVisitor visitor;
+  public SLAManager(Config config) {
+    this.config = config;
+  }
 
+  /**
+   * Load the defaults config
+   *
+   * @param heronHome, directory of heron home
+   * @param configPath, directory containing the config
+   * @param releaseFile, release file containing build information
+   * <p>
+   * return config, the defaults config
+   */
+  protected static Config defaultConfigs(String heronHome, String configPath, String releaseFile) {
+    Config config = Config.newBuilder()
+        .putAll(ClusterDefaults.getDefaults())
+        .putAll(ClusterDefaults.getSandboxDefaults())
+        .putAll(ClusterConfig.loadConfig(heronHome, configPath, releaseFile))
+        .build();
+    return config;
+  }
 
+  /**
+   * Load the override config from cli
+   *
+   * @param overrideConfigPath, override config file path
+   * <p>
+   * @return config, the override config
+   */
+  protected static Config overrideConfigs(String overrideConfigPath) {
+    Config config = Config.newBuilder()
+        .putAll(ClusterConfig.loadOverrideConfig(overrideConfigPath))
+        .build();
+    return config;
+  }
+
+  /**
+   * Load the config parameters from the command line
+   *
+   * @param cluster, name of the cluster
+   * @param role, user role
+   * @param environ, user provided environment/tag
+   * @param verbose, enable verbose logging
+   * @return config, the command line config
+   */
+  protected static Config commandLineConfigs(String cluster,
+                                             String role,
+                                             String environ,
+                                             Boolean verbose) {
+    Config config = Config.newBuilder()
+        .put(Keys.cluster(), cluster)
+        .put(Keys.role(), role)
+        .put(Keys.environ(), environ)
+        .put(Keys.verbose(), verbose)
+        .build();
+
+    return config;
+  }
+
+  // Print usage options
+  private static void usage(Options options) {
+    HelpFormatter formatter = new HelpFormatter();
+    formatter.printHelp(SLAManager.class.getSimpleName(), options);
+  }
+
+  // Construct all required command line options
+  private static Options constructOptions() {
+    Options options = new Options();
+
+    Option cluster = Option.builder("c")
+        .desc("Cluster name in which the topology needs to run on")
+        .longOpt("cluster")
+        .hasArgs()
+        .argName("cluster")
+//        .required()
+        .build();
+
+    Option role = Option.builder("r")
+        .desc("Role under which the topology needs to run")
+        .longOpt("role")
+        .hasArgs()
+        .argName("role")
+//        .required()
+        .build();
+
+    Option environment = Option.builder("e")
+        .desc("Environment under which the topology needs to run")
+        .longOpt("environment")
+        .hasArgs()
+        .argName("environment")
+//        .required()
+        .build();
+
+    Option heronHome = Option.builder("d")
+        .desc("Directory where heron is installed")
+        .longOpt("heron_home")
+        .hasArgs()
+        .argName("heron home dir")
+//        .required()
+        .build();
+
+    Option configFile = Option.builder("p")
+        .desc("Path of the config files")
+        .longOpt("config_path")
+        .hasArgs()
+        .argName("config path")
+//        .required()
+        .build();
+
+    Option configOverrides = Option.builder("o")
+        .desc("Command line override config path")
+        .longOpt("override_config_file")
+        .hasArgs()
+        .argName("override config file")
+        .build();
+
+    Option releaseFile = Option.builder("b")
+        .desc("Release file name")
+        .longOpt("release_file")
+        .hasArgs()
+        .argName("release information")
+        .build();
+
+    Option verbose = Option.builder("v")
+        .desc("Enable debug logs")
+        .longOpt("verbose")
+        .build();
+
+    options.addOption(cluster);
+    options.addOption(role);
+    options.addOption(environment);
+    options.addOption(heronHome);
+    options.addOption(configFile);
+    options.addOption(configOverrides);
+    options.addOption(releaseFile);
+    options.addOption(verbose);
+
+    return options;
+  }
+
+  // construct command line help options
+  private static Options constructHelpOptions() {
+    Options options = new Options();
+    Option help = Option.builder("h")
+        .desc("List all options and their description")
+        .longOpt("help")
+        .build();
+
+    options.addOption(help);
+    return options;
+  }
+
+  public static void main(String[] args) throws Exception {
+    Options options = constructOptions();
+    Options helpOptions = constructHelpOptions();
+    CommandLineParser parser = new DefaultParser();
+    // parse the help options first.
+    CommandLine cmd = parser.parse(helpOptions, args, true);
+
+    if (cmd.hasOption("h")) {
+      usage(options);
+      return;
+    }
+
+    try {
+      // Now parse the required options
+      cmd = parser.parse(options, args);
+    } catch (ParseException e) {
+      usage(options);
+      throw new RuntimeException("Error parsing command line options: ", e);
+    }
+
+    Boolean verbose = false;
+    Level logLevel = Level.INFO;
+    if (cmd.hasOption("v")) {
+      logLevel = Level.ALL;
+      verbose = true;
+    }
+
+    // init log
+    LoggingHelper.loggerInit(logLevel, false);
+
+    String cluster = cmd.getOptionValue("cluster");
+    String role = cmd.getOptionValue("role");
+    String environ = cmd.getOptionValue("environment");
+    String heronHome = cmd.getOptionValue("heron_home");
+    String configPath = cmd.getOptionValue("config_path");
+    String overrideConfigFile = cmd.getOptionValue("override_config_file");
+    String releaseFile = cmd.getOptionValue("release_file");
+
+    // first load the defaults, then the config from files to override it
+    // next add config parameters from the command line
+    // load the topology configs
+
+    // build the final config by expanding all the variables
+    Config config = Config.expand(
+        Config.newBuilder()
+            .putAll(defaultConfigs(heronHome, configPath, releaseFile))
+            .putAll(overrideConfigs(overrideConfigFile))
+            .putAll(commandLineConfigs(cluster, role, environ, verbose))
+            .build());
+
+    LOG.fine("Static config loaded successfully ");
+    LOG.fine(config.toString());
+
+    SLAManager slaManager = new SLAManager(config);
+    slaManager.run();
+  }
+
+  private void run() {
+    System.out.println("Running the SLA manager");
+    System.out.println(config);
+  }
 }
